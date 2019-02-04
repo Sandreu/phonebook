5834035c37989dc095578cfcd756cad6a78c2d11
sebastien_andreu <deadyman@free.fr>
2019-02-04 23:07:53 +0000

    with fetch

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Phonebook Challenge
+
+## Working result
+
+You can check the running Single Page App here [https://sandreu.github.io/phonebook/](https://sandreu.github.io/phonebook/)
+
+## Make it work locally
+
+You can make it work with the following commands
+
+```
+git clone https://github.com/Sandreu/phonebook.git phonebook
+cd phonebook/client
+npm install
+npm start
+```
+
+Then it should work on your localhost:8080
--- client/src/tab/data-process.js
+++ client/src/tab/data-process.js
@@ -3,64 +3,51 @@ import { useSharedState } from '../shared-states'
 
 import View from './view'
 
-const data = {
-  "contacts": [
-  	{
-			"name": "Oleta Level",
-			"phone_number": "+442032960159",
-			"address": "10 London Wall, London EC2M 6SA, UK"
-		}, {
-			"name": "Maida Harju",
-			"phone_number": "+442032960899",
-			"address": "Woodside House, 94 Tockholes Rd, Darwen BB3 1LL, UK"
-		}, {
-			"name": "Lia Pigford",
-			"phone_number": "+442032960182",
-			"address": "23 Westmorland Cl, Darwen BB3 2TQ, UK"
-		}, {
-			"name": "Ghislaine Darden",
-			"phone_number": "+442032960427",
-			"address": "20-24 Knowlesly Rd, Darwen BB3 2NE, UK"
-		}, {
-			"name": "Jana Spitler",
-			"phone_number": "+442032960370",
-			"address": "4 St Lucia Cl, Darwen BB3 0SJ, UK"
-		}, {
-			"name": "Dolly Detweiler",
-			"phone_number": "+442032960977",
-			"address": "18 Johnson Rd, Darwen BB3, UK"
-		}, {
-			"name": "Stanley Vanderhoof",
-			"phone_number": "+442032960000",
-			"address": "17 Anchor Ave, Darwen BB3 0AZ, UK"
-		}, {
-			"name": "Adan Milian",
-			"phone_number": "+442032960011",
-			"address": "20 Ellerbeck Rd, Darwen BB3 3EX, UK"
-		}, {
-			"name": "Marivel Molina",
-			"phone_number": "+442032960013",
-			"address": "Tockholes Rd, Darwen BB3, UK"
-		}, {
-			"name": "Kris Everett",
-			"phone_number": "+442032960012",
-			"address": "Pinewood, Tockholes Rd, Darwen BB3 1JY, UK"
-		}
-	]
+let source = null
+function initSource(data) {
+	source = data.contacts.map((e, i) => {
+	  return Object.assign({
+	    _index: i,
+	    _search_name: e.name.toLowerCase(),
+	    _search_address: e.address.toLowerCase(),
+	  }, e)
+	})
 }
 
-const source = data.contacts.map((e, i) => {
-  return Object.assign({
-    _index: i,
-    _search_name: e.name.toLowerCase(),
-    _search_address: e.address.toLowerCase(),
-  }, e)
-})
+const dataSource = 'http://www.mocky.io/v2/581335f71000004204abaf83'
+if (window) {
+	if (window.fetch) {
+		window.fetch(dataSource)
+			.then(resp => resp.json())
+			.then(initSource)
+			.catch(e => {
+				alert(`Error while trying to fetch source\n${dataSource}\n\n${e.message}`)
+			})
+	} else {
+		const req = new XMLHttpRequest();
+	
+		req.onreadystatechange = function(event) {
+		    // XMLHttpRequest.DONE === 4
+		    if (this.readyState === XMLHttpRequest.DONE) {
+		        if (this.status === 200) {
+		            initSource(JSON.parse(this.responseText));
+		        } else {
+							alert(`Error while trying to XMLHttpRequest source\n${dataSource}`)
+		        }
+		    }
+		};
+		
+		req.open('GET', dataSource, true);
+		req.send(null);
+	}
+}
 
 export default () => {
   let search = useSharedState('search').toLowerCase()
   let sorting = useSharedState('sorting')
   let filters = useSharedState('filters')
+  
+  if (!source) return 'Loading data'
   
   let processed = source
   
