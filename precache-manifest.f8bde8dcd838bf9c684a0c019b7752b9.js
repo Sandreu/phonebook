4dc280ac174e3616c159a33b3944a711cdf8cf42
sebastien_andreu <deadyman@free.fr>
2019-02-04 22:24:57 +0000

    second try

---
+++ precache-manifest.f8bde8dcd838bf9c684a0c019b7752b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phonebook/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e6f93ec87d0359e1e859",
+    "url": "/phonebook/static/js/runtime~main.e6f93ec8.js"
+  },
+  {
+    "revision": "a221c1bfa6c1642f1676",
+    "url": "/phonebook/static/js/main.a221c1bf.chunk.js"
+  },
+  {
+    "revision": "a5b1db625cb998cc3bf9",
+    "url": "/phonebook/static/js/1.a5b1db62.chunk.js"
+  },
+  {
+    "revision": "a221c1bfa6c1642f1676",
+    "url": "/phonebook/static/css/main.5fb11a5c.chunk.css"
+  },
+  {
+    "revision": "9b266a7f5003332fa8dba01f5644652f",
+    "url": "/phonebook/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.692c3114cbdf67471a21580c073fc503.js"
+  "/phonebook/precache-manifest.f8bde8dcd838bf9c684a0c019b7752b9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/phonebook/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
